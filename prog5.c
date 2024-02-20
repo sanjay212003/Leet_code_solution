d891ae7392d1e9b0b331e68e3aa3bbb15d109292
sanjay212003 <139911764+sanjay212003@users.noreply.github.com>
2024-02-20 15:20:42 +0530

    Create prog5.c

---
+++ prog5.c
@@ -0,0 +1,15 @@
+//Reverse Linked List
+struct ListNode* reverseList(struct ListNode* head){
+
+    struct ListNode* temp=NULL;
+    struct ListNode* previous=NULL;
+
+    while(head!=NULL)
+    {
+        temp=head->next;
+        head->next=previous;
+        previous=head;
+        head=temp;
+    }
+    return previous;
+}
